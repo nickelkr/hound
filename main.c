5813e0cd5aab763426fa3ba746f497b31e332621
Kyle Nickel <nickelkr@gmail.com>
2015-10-25 12:20:00 -0400

    Create method

--- main.c
+++ main.c
@@ -1,28 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include <pcap/pcap.h>
+#include <unistd.h>
 
-pcap_if_t* getDevices() {
-    char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_if_t *interfaces = NULL;
+struct options {
+    char *interface;
+};
 
-    // find all of the devices on the machine
-    int code = pcap_findalldevs(&interfaces, errbuf);
+typedef char err[PCAP_ERRBUF_SIZE];
+typedef struct options options_t;
 
-    // check the codes to ensure we didn't receive an error
-    // error received and should be in errbuf
-    if (code == -1) {
-        printf("Error: %s\n", errbuf);
-    } else if (code != 0) {
-        printf("Uknown code received: %d\n", code);
+void setOpts(options_t *opts, int argc, char **argv) {
+    int c;
+    opts->interface = NULL;
+
+    while((c = getopt(argc, argv, "d:")) != -1) {
+        switch (c) {
+            case 'd':
+                opts->interface = optarg;
+                break;
+            case '?':
+                if (optopt == 'd')
+                    fprintf(stderr, "Options -%c requires a argument.\n", optopt);
+                else if (isprint(optopt))
+                    fprintf(stderr, "Uknown options -%c.\n", optopt);
+                else
+                    fprintf(stderr, "Uknown option (non-printable character).\n");
+                exit(1);
+            default:
+                break;
+        }
     }
-
-    return interfaces;
 }
 
-int main() {
-    printf("Welcome to Hound\n");
+void notify(char *err) {
+    fprintf(stderr, "Error: %s", err);
+    exit(1);
+}
+
+pcap_t* create(char *device) {
+    err err = "";
+    pcap_t *interface;
+
+    if (device == NULL)
+        device = pcap_lookupdev(err);
+    if (device == NULL)
+        notify(err);
+
+    interface = pcap_create(device, err);
+    if (interface == NULL)
+        notify(err);
+
+    return interface;
+}
 
 
+int main(int argc, char **argv) {
+    options_t *opts = {NULL};
+    pcap_t *interface;
+
+    setOpts(opts, argc, argv);
+    interface = create(opts->interface);
 
 
     return 0;
